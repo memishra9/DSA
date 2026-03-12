b7b61f8c6f4d565f36bbb496a49fae56e5d5373f
memishra9 <38108646+memishra9@users.noreply.github.com>
2026-03-12 08:23:51 +0530

    example implementation

--- merge_sorted_array.py
+++ merge_sorted_array.py
@@ -1,7 +1,7 @@
 from typing import List
+class Solution:
 
-
-def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         p1=m-1
         p2=n-1
         writpos=m+n-1
@@ -14,4 +14,12 @@ def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
                 nums1[writpos]= nums2[p2]
                 p2-=1
             writpos-=1
-        
+        return nums1
+
+# Example usage:
+s=Solution()
+nums1 = [1, 2, 3,0,0,0]
+m = 3
+nums2 = [2, 5, 6]
+n = 3
+print(s.merge(nums1, m, nums2, n))        
